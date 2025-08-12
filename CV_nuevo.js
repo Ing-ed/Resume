3511480415744e304b800c5dbb1d5027dea4730d
Ing-ed <emidovichi@gmail.com>
2025-08-12 16:02:21 -0300

    se unifica ahora los 4 CV para manejar parametros url y que devuelva lo que se pide

--- CV_nuevo.js
+++ CV_nuevo.js
@@ -1,32 +1,43 @@
 import express from 'express'
 import { Router } from 'express';
-import handlebars from 'express-handlebars'
+import Handlebars from 'express-handlebars'
 import __dirname from './utils.js';
 import path from 'path';
 import { ReadText } from './functions/text.js';
 import { KeyObject } from 'crypto';
+import { text } from 'stream/consumers';
+import { title } from 'process';
 
 const app = express();
 const router = Router()
-app.engine('handlebars',handlebars.engine())
+app.engine('handlebars',Handlebars.engine())
 app.use(express.static(path.join(__dirname,'public')))
 app.set('views',__dirname+'/views')
 app.set('view engine','handlebars')
 app.use("/",router)
 
 
-router.get("/",async (req, res) =>{
+
+
+
+router.get("/:clase&:lang",async (req, res) =>{
+    const clase = req.params.clase
+    console.log(clase, "clase")
+    const lang = req.params.lang
+    console.log(lang, "Lang")
     console.log("aca")
-    const about = await ReadText('about.txt')
-    const prof = await JSON.parse((await ReadText('professional/professional.json')))
-    const education = await JSON.parse(await ReadText('education/education.json'))
-    const background = await JSON.parse(await ReadText('background/background.json'))
+    const texts = await JSON.parse((await ReadText('text.json')))
+    console.log(texts['spanish']['professional']['title']['engineer'])
+    console.log(texts['spanish']['skills']['engineer'])
     
     res.render('CV_nuevo',{
-        about:about,
-        prof:prof,
-        education:education,
-        background:background,
+        clase: (clase === 'eng'? true : false),
+        skills:texts[`${lang === "en"?'english':"spanish"}`]['skills'],
+        presentation:texts[`${lang === "en"?'english':"spanish"}`]['presentation'],
+        about:texts[`${lang === "en"?'english':"spanish"}`]['about'],
+        prof:texts[`${lang === "en"?'english':"spanish"}`]['professional'],
+        education:texts[`${lang === "en"?'english':"spanish"}`]['education'],
+        background:texts[`${lang === "en"?'english':"spanish"}`]['background']
         })
 })
 
